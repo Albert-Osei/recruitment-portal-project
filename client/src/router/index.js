0488cd16e7645121f6c652f19c0312002a632501
Albert Osei <73245719+Albert-Osei@users.noreply.github.com>
2021-06-15 14:02:34 +0000

    admin login page  added

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import AdminLogin from '../views/AdminLogin.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'AdminLogin',
+    component: AdminLogin
   },
   {
     path: '/about',
---
+++ client/src/views/AdminLogin.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="adminlogin">
+    <div class="desktop-background">
+      <img src="../assets/desktop_background.svg" alt="">
+    </div>
+    <form action="" class="login-box">
+      <div class="logo">
+        <img src="../assets/logo.svg" alt="enyata logo" />
+      </div>
+      <h2 class="login-header">Admin Log In</h2>
+      <label for="email">Email Address</label>
+      <input class="emailfield" type="email" name="email" />
+      <label for="password">Password</label>
+      <input class="passwordfield" type="password" name="password" />
+      <button class="btn">Sign In</button>
+    </form>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'AdminLogin', 
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;1,100;1,300;1,400&display=swap');
+
+*, html {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #7557D3;
+}
+
+.desktop-background {
+  position: absolute;
+  right: 0;
+  top: 10%;
+}
+
+.login-box {
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 30%;
+  left: 40%;
+}
+
+.logo {
+  width: 110px;
+  height: 21px;
+  margin: auto;
+}
+
+.logo img {
+  width: 100%;
+  height: 100%;
+}
+
+.login-header {
+  font-family: 'Lato';
+  font-style: italic;
+  font-weight: 500;
+  font-size: 24px;
+  color: #FFFFFF;
+  text-align: center;
+  margin: 24px 0 58px;
+}
+
+label {
+  font-family: 'Lato';
+  font-weight: normal;
+  font-size: 14px;
+  color: #FFFFFF;
+}
+
+input {
+  border: 1.5px solid #FFFFFF;
+  border-radius: 4px;
+  width: 379px;
+  height: 48px;
+  background: none;
+  margin-bottom: 30px;
+}
+
+.btn {
+  width: 379px;
+  height: 50px;
+  background: #FFFFFF;
+  border-radius: 4px;
+  font-family: 'Lato';
+  font-weight: bold;
+  font-size: 16px;
+  color: #7557D3;
+  border: none;
+}
+
+</style>
